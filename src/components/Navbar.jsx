53e88a6a8804028f240034f01b62e44c0a5f28c0
HARISH-ANJAC <23pca121@anjaconline.org>
2025-07-21 14:06:42 +0530

    changes1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> HARISH PRABHU </span>{" "}
+            <span className="text-glow text-foreground"> HP</span>{" "}
             Portfolio
           </span>
         </a>
