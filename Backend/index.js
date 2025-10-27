18c1ef08a0200d17280dcc09ca9065f5e5833dad
HARISH PRABHU <112898522+Harish-Prabhu-Developer@users.noreply.github.com>
2025-10-27 12:27:30 +0530

    Modify email transporter settings
    Updated email transporter configuration to include requireTLS and increased connection timeout.

--- Backend/index.js
+++ Backend/index.js
@@ -33,14 +33,15 @@ app.post("/portfolio/send-email", async (req, res) => {
     const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true for port 465, false for port 587
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  requireTLS: true,
-  connectionTimeout: 10000 // optional, set timeout in ms
+  connectionTimeout: 20000 // 20 seconds, optional
 });
+
 
 
     // Email content
